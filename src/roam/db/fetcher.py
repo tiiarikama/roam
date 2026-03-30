462ddd97372cb7fd587c0eed341f89d41873b72f
tiiarikama <tiiarikama@hotmail.com>
2026-03-30 13:49:12 -0700

    feat: implement base request handler for NPS API + handler for NPS API /park endpoint

---
+++ src/roam/db/fetcher.py
@@ -0,0 +1,34 @@
+import requests
+from roam.config import NPS_API_KEY, NPS_BASE_URL, TARGET_PARKS
+
+
+# private, shared base request handler for NPS API 
+def _nps_get(endpoint: str, params: dict) -> dict:
+    params["api_key"] = NPS_API_KEY
+    response = requests.get(f"{NPS_BASE_URL}/{endpoint}", params=params)
+    response.raise_for_status()
+    return response.json()
+
+# NPS API call for core park metadata and description
+def fecth_park_info(park_code: str) -> dict:
+    response = _nps_get("parks", {"parkCode": park_code, "limit": 1})
+
+    if not response["data"]:
+        raise ValueError(f"No park found for code: {park_code}")
+    
+    park_data = response["data"][0]
+
+    return {
+        "park_code": park_code,
+        "name": park_data.get("fullName", ""),
+        "description": park_data.get("description", ""),
+        "states": park_data.get("states", ""),
+        "designation": park_data.get("designation", ""),
+        "weather_info": park_data.get("weatherInfo"),
+        "directions_info": park_data.get("directionsInfo"),
+        "activities": [activity["name"] for activity in park_data.get("activities", [])],
+        "topics": [topic["name"] for topic in park_data.get("topics", [])],
+        "entrance_fees": park_data.get("entranceFees", []),
+        "operating_hours": park_data.get("operatingHours", []),
+    }
+
